edba2558559c9eb17bf1e215f1e896e1bfa58b75
Unknown <Deadstylebp@gmail.com>
2020-09-19 17:04:21 -0500

    Update server settings

--- src/index.js
+++ src/index.js
@@ -81,8 +81,8 @@ server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
-const isTest = !!process.env.TEST_DATABASE_URL;
-const isProduction = process.env.NODE_ENV === 'production';
+// const isTest = !!process.env.TEST_DATABASE_URL;
+// const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 8000;
 
 connectDb().then(async () => {
